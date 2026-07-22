395e608b6f565a657b5d34d3e703d3c3980bd6e8
Leigh Hunt <leigh.hunt@lynker-analytics.com>
2026-07-22 22:33:10 +1200

    switching to wsl

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,1 +1,12 @@
-/home/leigh/.config/omarchy/current/theme/neovim.lua
+return {
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000,
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "tokyonight-night",
+		},
+	},
+}
